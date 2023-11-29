f1283d06b35cbe996e15eebbec7db12cd86a4cab
samib93 <sami.barkaoui93290@gmail.com>
2023-11-29 13:39:23 +0100

    statistique

--- OneDrive/Bureau/B3 DEV/Shift-sami/tailwind.config.js
+++ OneDrive/Bureau/B3 DEV/Shift-sami/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'custom-color': '#192F48',
+      },
+      fontFamily: {
+        'gibson': ['Gibson Regular', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
