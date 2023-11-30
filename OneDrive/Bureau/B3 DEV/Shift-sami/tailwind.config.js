00d14684406334c1812421cd50fbae84d8437adc
samib93 <sami.barkaoui93290@gmail.com>
2023-11-30 16:52:56 +0100

    correction page d'accueil

--- OneDrive/Bureau/B3 DEV/Shift-sami/tailwind.config.js
+++ OneDrive/Bureau/B3 DEV/Shift-sami/tailwind.config.js
@@ -7,8 +7,44 @@ module.exports = {
         'custom-color': '#192F48',
       },
       fontFamily: {
-        'gibson': ['Gibson Regular', 'sans-serif'],
+        "canada-type-gibson": ['"canada-type-gibson"', "sans-serif"],
+        "canada-type-gibson-italic": [
+          '"canada-type-gibson"',
+          "sans-serif",
+          { style: "italic" },
+        ],
+        "canada-type-gibson-medium": [
+          '"canada-type-gibson"',
+          "sans-serif",
+          { weight: "500" },
+        ],
+        "canada-type-gibson-medium-italic": [
+          '"canada-type-gibson"',
+          "sans-serif",
+          { weight: "500", style: "italic" },
+        ],
+        "canada-type-gibson-semibold": [
+          '"canada-type-gibson"',
+          "sans-serif",
+          { weight: "600" },
+        ],
+        "canada-type-gibson-semibold-italic": [
+          '"canada-type-gibson"',
+          "sans-serif",
+          { weight: "600", style: "italic" },
+        ],
+        "canada-type-gibson-bold": [
+          '"canada-type-gibson"',
+          "sans-serif",
+          { weight: "700" },
+        ],
+        "canada-type-gibson-bold-italic": [
+          '"canada-type-gibson"',
+          "sans-serif",
+          { weight: "700", style: "italic" },
+        ],
       },
+      
     },
   },
   plugins: [],
